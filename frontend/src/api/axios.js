e73c9687a9b00593102789dbac3de16b4e7a08c1
nehal singh <nehalsingh903@gmail.com>
2026-05-28 21:52:56 +0530

    fix: update API config for Vercel

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
+const BASE_URL =
+  import.meta.env.VITE_API_URL ||
+  "http://localhost:5000";
 
+  
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${BASE_URL}/api`,
 });
 
 
